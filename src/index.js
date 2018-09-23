5601c1c4523342009e3b7ad5a07f420b6edd15b8
Fancy322 <vav10041997@gmail.com>
2018-09-23 11:20:55 +0300

    My commit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,51 @@
 class Sorter {
   constructor() {
+    this.array = [];
+      this.comparator = (a, b) => (a-b);
     // your implementation
   }
 
   add(element) {
+    this.array.push(element);
     // your implementation
   }
 
   at(index) {
+    return this.array[index];
     // your implementation
   }
 
   get length() {
+    return this.array.length;
     // your implementation
   }
 
   toArray() {
+    return this.array;
     // your implementation
   }
 
   sort(indices) {
+    let subArray = [];
+      const sortedIndices = indices.sort(this.comparator);
+      
+      if (!Array.isArray(indices)) {
+          return this.array;
+      }
+      
+      sortedIndices.forEach(value => subArray.push(this.array[value]));
+      
+      subArray = subArray.sort(this.customComparator || this.comparator);
+    
+      sortedIndices.forEach((value, index) => this.array[value] = subArray[index] );
+  }
+
     // your implementation
   }
 
-  setComparator(compareFunction) {
+  setComparator(compareFunction) 
+    this.customComparator = compareFunction;
     // your implementation
-  }
-}
+  
 
-module.exports = Sorter;
+
